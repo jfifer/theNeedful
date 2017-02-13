8abdcb0ab3b144eafcd52a9506449a5ec4125b31
Joe Fifer <jfifer@coredial.com>
2017-02-13 14:27:29 -0500

    better version

--- getDedicatedExtensions.sql
+++ getDedicatedExtensions.sql
@@ -1,49 +1,74 @@
-CREATE PROCEDURE getOverallExtensions2017()
-BEGIN
-
-DECLARE v_std2017,
-        v_cloud2017,
-        v_sip2017 INT;
-
-SELECT COUNT(b.branchId)
-  INTO v_sip2017
+IPTRUNK#####
+SELECT concat(serv.name, ":  ", COUNT(s.sipTrunkId))
   FROM sipTrunk s
   LEFT JOIN branch b ON (s.branchId=b.branchId)
   LEFT JOIN customer c ON (b.customerId=c.customerId)
-  LEFT JOIN reseller r ON (c.resellerId=r.resellerId)
-  LEFT JOIN serverGroup sg ON (r.resellerId=sg.resellerId)
+  LEFT JOIN server serv ON (serv.serverId=b.featureServerId)
  WHERE c.statusId=1
- AND sg.serverGroupId NOT IN (2,3,6,7,8,33,34,48,76,84,101,133)
+ AND serv.serverGroupId NOT IN (1,2,3,4,5,6,7,8,33,34,48,54,76,84,101,102,133)
+ AND c.originalActivationDate < '2018-01-01'
+GROUP BY serv.serverId;
+
+#####SIPTRUNK TOTAL#####
+SELECT COUNT(s.sipTrunkId)
+  FROM sipTrunk s
+  LEFT JOIN branch b ON (s.branchId=b.branchId)
+  LEFT JOIN customer c ON (b.customerId=c.customerId)
+  LEFT JOIN server serv ON (serv.serverId=b.featureServerId)
+ WHERE c.statusId=1
+ AND serv.serverGroupId NOT IN (1,2,3,4,5,6,7,8,33,34,48,54,76,84,101,102,133)
  AND c.originalActivationDate < '2018-01-01';
 
-SELECT COUNT(e.extensionId)
-  INTO v_std2017 
+#####STANDARD DETAIL#####
+SELECT concat(s.name, ":  ", COUNT(e.extensionId))
   FROM extension e
   LEFT JOIN branch b ON (e.branchId=b.branchId)
   LEFT JOIN customer c ON (b.customerId=c.customerId)
-  LEFT JOIN reseller r ON (r.resellerId = c.resellerId)
-  LEFT JOIN serverGroup sg ON (sg.resellerId = c.resellerId)
+  LEFT JOIN server s ON (b.featureServerId = s.serverId)
  WHERE c.statusId=1
-   AND sg.serverGroupId NOT IN (2,3,6,7,8,33,34,48,76,84,101,133)
+   AND s.serverGroupId NOT IN (1,2,3,4,5,6,7,8,33,34,48,54,76,84,101,102,133)
+   AND e.extensionTypeId=1
+   AND e.isFreeExtension=0
+   AND c.companyName NOT LIKE '%test%'
+   AND c.companyName NOT LIKE '%demo%'
+   AND c.originalActivationDate < '2018-01-01'
+GROUP BY s.serverId;
+
+#####STANDARD TOTAL#####
+SELECT COUNT(e.extensionId)
+  FROM extension e
+  LEFT JOIN branch b ON (e.branchId=b.branchId)
+  LEFT JOIN customer c ON (b.customerId=c.customerId)
+  LEFT JOIN server s ON (b.featureServerId = s.serverId)
+ WHERE c.statusId=1
+   AND s.serverGroupId NOT IN (1,2,3,4,5,6,7,8,33,34,48,54,76,84,101,102,133)
    AND e.extensionTypeId=1
    AND e.isFreeExtension=0
    AND c.companyName NOT LIKE '%test%'
    AND c.companyName NOT LIKE '%demo%'
    AND c.originalActivationDate < '2018-01-01';
 
-SELECT COUNT(e.extensionId)
-  INTO v_cloud2017
+#####CLOUD DETAIL#####
+SELECT concat(s.name, ":  ", COUNT(e.extensionId))
   FROM extension e
   LEFT JOIN branch b ON (e.branchId=b.branchId)
   LEFT JOIN customer c ON (b.customerId=c.customerId)
-  LEFT JOIN reseller r ON (r.resellerId = c.resellerId)
-  LEFT JOIN serverGroup sg ON (c.resellerId = sg.resellerId)
+  LEFT JOIN server s ON (b.featureServerId=s.serverId)
  WHERE c.statusId=1
-   AND sg.serverGroupId NOT IN (2,3,6,7,8,33,34,48,76,84,101,133)
+   AND s.serverGroupId NOT IN (1,2,3,4,5,6,7,8,33,34,48,54,76,84,101,102,133)
+   AND e.extensionTypeId=2
+   AND e.isFreeExtension=0
+   AND c.originalActivationDate < '2018-01-01'
+GROUP BY s.serverId;
+
+#####CLOUD TOTAL#####
+SELECT COUNT(e.extensionId)
+  FROM extension e
+  LEFT JOIN branch b ON (e.branchId=b.branchId)
+  LEFT JOIN customer c ON (b.customerId=c.customerId)
+  LEFT JOIN server s ON (b.featureServerId=s.serverId)
+ WHERE c.statusId=1
+   AND s.serverGroupId NOT IN (1,2,3,4,5,6,7,8,33,34,48,54,76,84,101,102,133)
    AND e.extensionTypeId=2
    AND e.isFreeExtension=0
    AND c.originalActivationDate < '2018-01-01';
-
-SELECT '2017',v_std2017,v_cloud2017,v_sip2017;
-
-END;
